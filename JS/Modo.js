2ff70ebf0d21410706edd4de5ac8befa08b02de9
hermelen <Hermelen>
2018-05-28 12:07:39 +0200

    exercise done

--- JS/Modo.js
+++ JS/Modo.js
@@ -1,21 +1,8 @@
+// :') j'adore les :) c'est trop :D !
+
+
 btn = document.getElementById("mod-btn");
 btn.addEventListener('click', beautify);
-
-// var smiley = [":')", ":)", ":D"];
-//
-// for (var i = 0; i < smiley.length; i++) {
-//   console.log("smiley["+ i + "] = " + smiley[i]);
-// }
-
-// function beautify(){
-//   modTxt = document.getElementById('mod-txt').value;
-//   for (var i = 0; i < smiley.length; i++) {
-//     console.log("smiley["+ i + "] = " + smiley[i]);
-//     var n = modTxt.replace(smiley[i], "-");
-//     console.log(n);
-//   }
-//
-// }
 
 var smiley1 = ":')";
 var smiley2 = ":)";
@@ -23,7 +10,11 @@ var smiley3 = ":D";
 
 function beautify() {
   modTxt = document.getElementById('mod-txt').value;
-  var n = modTxt.replace(/smiley1|smiley2|smiley3/, "--");
-  console.log(n);
+  var modif = modTxt.replace(smiley1, "--");
+  modif = modif.replace(smiley2, "--");
+  modif = modif.replace(smiley3, "--");
+  console.log(modif);
 
+  result = document.getElementById('result');
+  result.innerText = modif;
 }
